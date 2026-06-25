570cabdf5310991823e1256d7b6e91af80923a8c
cindysoto148-design <cindysoto148@gmail.com>
2026-06-25 16:05:27 -0600

    Agregar proyecto.py y proyecto.md

---
+++ proyecto.md
@@ -0,0 +1,8 @@
+# Proyecto de Control de Versiones
+
+Nombre: Cindy Maciel
+Apellido: Soto Moya
+Grupo: (Dominical-G1)
+Año: 2026
+Fecha: 25/06/2026
+Hora: 4:00 PM
---
+++ proyecto.py
@@ -0,0 +1,10 @@
+# Proyecto de Control de Versiones
+
+nombre = "Cindy Maciel"
+apellido = "Soto Moya"
+grupo = "Domonical-C1"
+anio = 2026
+fecha = "25/06/2026"
+hora = "4:00 PM"
+
+print(nombre, apellido)
